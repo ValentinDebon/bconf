dd8bf1baa7c58708c0ce7c1e914165cab76bd18f
Valentin Debon <valentin.debon@heylelos.org>
2023-03-20 17:19:55 +0100

    Simplified makefile
    Removed src directory and examples

--- bconf.mk
+++ bconf.mk
@@ -1,25 +1,25 @@
-mkconf-objs:=src/mkconf.o y.tab.o lex.yy.o
+mkconf-objs:=mkconf.o y.tab.o lex.yy.o
 
-$(mkconf-objs): CPPFLAGS+=-D_GNU_SOURCE -I$(objdir) -I$(srcdir)/src
+$(mkconf-objs): CPPFLAGS+=-D_GNU_SOURCE -I$(objdir) -I$(srcdir)
 
-src/mkconf.o: CPPFLAGS+= \
+mkconf.o: CPPFLAGS+= \
 	-DCONFIG_DEFAULT_OUTPUT_TEMPLATE='"$(CONFIG_DEFAULT_OUTPUT_TEMPLATE)"' \
 	-DCONFIG_DEFAULT_MAKEFILE_TEMPLATE='"$(CONFIG_DEFAULT_MAKEFILE_TEMPLATE)"' \
 	-DCONFIG_DEFAULT_OUTPUT='"$(CONFIG_DEFAULT_OUTPUT)"' \
 	-DCONFIG_DEFAULT_BCONF='"$(CONFIG_DEFAULT_BCONF)"'
-src/mkconf.o: y.tab.h
+mkconf.o: y.tab.h
 
-y.tab.c: y.tab.h
-y.tab.h: $(srcdir)/src/bconf.y
+y.tab.h: y.tab.c
+y.tab.c: bconf.y
 	$(v-e) YACC $@
-	$(v-a) $(YACC) $(YFLAGS) -d $<
+	$(v-a) $(YACC) $(YFLAGS) -o y.tab.c -d $<
 
-lex.yy.c: $(srcdir)/src/bconf.l y.tab.h
+lex.yy.c: bconf.l y.tab.h
 	$(v-e) LEX $@
-	$(v-a) $(LEX) $(LFLAGS) $<
+	$(v-a) $(LEX) $(LFLAGS) -o $@ $<
 
 mkconf: $(mkconf-objs)
 
 host-bin+=mkconf
-host-data+=data/configure.in data/Makefile.in
+host-data+=configure.in Makefile.in
 clean-up+=$(host-bin) $(mkconf-objs) y.tab.c y.tab.h lex.yy.c
--- examples/arch-specific/bconf.mk
+++
@@ -1,8 +0,0 @@
-hello-x86_64:=hello.o
-ifneq ($(hello-$(host-arch)),)
-hello: CPPFLAGS:=-nostdinc
-hello: LDFLAGS:=-nostdlib
-hello: $(hello-$(host-arch))
-host-$(host-arch)+=$(hello-$(host-arch))
-host-bin+=hello
-endif
